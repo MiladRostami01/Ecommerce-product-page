326798f4272a634747d91d5e8034b5380d5fa071
Milad Rostami <boyka682000@gmail.com>
2021-10-23 16:33:51 +0330

    script.js is updated

--- js/script.js
+++ js/script.js
@@ -8,9 +8,15 @@ const overlay = document.querySelector('.mr-overlay')
 
 const plusBtn = document.querySelector('.plus-btn')
 const minesBtn = document.querySelector('.mines-btn')
-const  countLabel = document.querySelector('.count-lable')
+const labelCount = document.querySelector('.count-lable')
 
-let count = parseInt(countLabel.innerHTML)
+const addToCartBtn =document.querySelector('.add-to-cart')
+const cartCount = document.querySelector('.mr-cart-count')
+
+
+const cartResult = document.querySelector('.mr-cart-content')
+
+
 
 
 const info ={
@@ -47,11 +53,44 @@ overlay.addEventListener('click', ()=>{
 })
 
 plusBtn.addEventListener('click', () => {
-  count =  count + 1
-  countLabel.innerText = count
+  let plus = parseInt(labelCount.innerHTML)
+  plus =  plus + 1
+  labelCount.innerText = plus
 })
 
 minesBtn.addEventListener('click', () => {
-  if(count > 1) count =  count - 1
-  countLabel.innerText = count
+  let mines = parseInt(labelCount.innerHTML)
+  if(mines > 1) mines =  mines - 1
+  labelCount.innerText = mines
 })
+
+addToCartBtn.addEventListener('click', () => {
+  cartCount.innerHTML = parseInt(cartCount.innerHTML) + parseInt(labelCount.innerHTML)
+
+  labelCount.innerText = 1
+
+  //calc 
+
+  cartResult.innerHTML= `
+  <div class="mr-cart-result">
+    <div class="mr-product-img"><img src="images/image-product-1-thumbnail.jpg" alt=""></div>
+    <div class="mr-product-description">
+      <p>Fall Limited Edition Sneakers...</p>
+      <p>$125.00 × ${cartCount.innerHTML} <span>$${cartCount.innerHTML * 125}.00</span></p>
+    </div>
+    <a class="mr-product-delete"><svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg></a>
+  </div>
+  <button class="btn mr-checkout-btn">Checkout</button>
+  `
+
+  const deleteIcon = document.querySelector('.mr-product-delete')
+
+  deleteIcon.addEventListener('click', ()=> {
+    cartCount.innerHTML= 0
+    cartResult.innerHTML= `
+      <p class="mr-cart-content-empety">Your cart is empty.</p>
+    `
+
+  })
+})
+
